3b4b73a538385db632aa1d68f68a2f286b62f04c
joaobassiDenver <137333862+joaobassiDenver@users.noreply.github.com>
2025-03-19 00:16:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -87,6 +87,7 @@ if st.session_state.relatorio:
 if st.button("Salvar Relatório"):
     if st.session_state.relatorio:
         # Verifica se o arquivo já existe
+        print(os.get_cwd())
         if os.path.exists(db_arquivo):
             # Se o arquivo já existe, carrega o arquivo existente
             df_existente = pd.read_excel(db_arquivo)
